10da4023b1c2603e4c630c60bf6443251d3fda02
diegogirao <diegoramiro25@gmail.com>
2023-04-17 17:34:37 +0100

    Feat create hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Header() {
-	return <div>Header</div>
+	return (
+		<div className="hero min-h-screen bg-gray-500">
+			<h1 className="text-3xl text-red-900">Header</h1>
+		</div>
+	)
 }
