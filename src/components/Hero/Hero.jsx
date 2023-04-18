ffa48c076052051eb69032859d73d54ba5a07326
diegogirao <diegoramiro25@gmail.com>
2023-04-18 18:50:52 +0100

    Feat create hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Hero() {
+	return (
+		<div className="hero min-h-screen bg-gray-500">
+			<h1 className="text-3xl text-cyan-500">Hero</h1>
+		</div>
+	)
+}
