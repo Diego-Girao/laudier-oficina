f2a405eacdb0b182ca8b8b3212b4add21c4e4ca4
diegogirao <diegoramiro25@gmail.com>
2023-04-18 19:02:08 +0100

    Feat create section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,16 @@ import React from "react"
 export default function Hero() {
 	return (
 		<div className="hero min-h-screen bg-gray-500">
-			<h1 className="text-3xl text-cyan-500">Hero</h1>
+			<section className="justify-center text-white">
+				<h1 className="text-3xl font-bold">Hero</h1>
+				<p className="text-xl font-bold">Hi everyone!</p>
+				<span>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+					rem consequatur nihil, repellat iste, minus beatae recusandae eum
+					cumque vero totam deserunt tenetur placeat repudiandae sunt saepe
+					sapiente? Natus, quasi!
+				</span>
+			</section>
 		</div>
 	)
 }
