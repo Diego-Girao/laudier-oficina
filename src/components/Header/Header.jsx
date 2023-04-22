4b22a27766baeb78674835407a553c1b13125097
diegogirao <diegoramiro25@gmail.com>
2023-04-22 14:14:21 +0100

    Style testing menu and hovers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,29 @@
 import React from "react"
+import logo from "/src/assets/images/logo_oficina.svg"
 
 export default function Header() {
 	return (
-		<div className="navbar bg-base-100">
+		<div className="navbar bg-base-100 fixed shadow-md">
 			<div className="flex-1">
-				<a className="btn btn-ghost normal-case text-xl">Logo Oficina</a>
+				<a>
+					<img
+						className="btn-lg cursor-pointer hover:blur-sm duration-200 ease-in-out"
+						src={logo}
+					/>
+				</a>
 			</div>
 			<div className="flex-none">
-				<ul className="menu menu-horizontal px-1">
-					<li>
+				<ul className="menu menu-vertical lg:menu-horizontal px-1">
+					<li className="hover-bordered">
 						<a>Sobre</a>
 					</li>
-					<li>
+					<li className="hover:font-black">
 						<a>Serviços</a>
 					</li>
-					<li>
+					<li className="hover-bordered">
 						<a>L&A Autopeças</a>
 					</li>
-					<li>
+					<li className="hover-bordered">
 						<a>Contato</a>
 					</li>
 				</ul>
