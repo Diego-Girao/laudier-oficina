a8edc91f1a64467060eeb52f82869c8485d55f85
diegogirao <diegoramiro25@gmail.com>
2023-04-23 21:27:37 +0100

    Style change p and add new text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,13 +4,12 @@ export default function Hero() {
 	return (
 		<div className="hero min-h-screen bg-gray-500">
 			<section className="justify-center text-white">
-				<h1 className="text-3xl font-bold">Hero</h1>
-				<p className="text-xl font-bold">Hi everyone!</p>
-				<span>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-					rem consequatur nihil, repellat iste, minus beatae recusandae eum
-					cumque vero totam deserunt tenetur placeat repudiandae sunt saepe
-					sapiente? Natus, quasi!
+				<p className="text-5xl font-bold text-black">
+					Manutenção preventiva é aqui ! 👊{" "}
+				</p>
+				<span className="text-3xl">
+					Profissional qualificado, produtos de qualidade e com aquele precinho
+					e condições facilitadas! Venham conferir!
 				</span>
 			</section>
 		</div>
