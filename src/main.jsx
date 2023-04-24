1651acd00465554bf0c2c68fef231ac9eec986d6
diegogirao <diegoramiro25@gmail.com>
2023-04-24 20:24:30 +0100

    Feat create browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import About from "./routes/About/About"
+import Repair from "./routes/Repair/Repair"
+import Store from "./routes/Store/Store"
+import Contact from "./routes/Contact/Contact"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/repair",
+				element: <Repair />,
+			},
+			{
+				path: "/store",
+				element: <Store />,
+			},
+			{
+				path: "/contact",
+				element: <Contact />,
+			},
+		],
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
