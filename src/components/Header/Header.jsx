c915250b33ac4c4c3bc49aae12ced257bc47f652
diegogirao <diegoramiro25@gmail.com>
2023-04-24 20:24:57 +0100

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,5 @@
 import React from "react"
-import logo from "/src/assets/images/logo_oficina.svg"
 
 export default function Header() {
-	return (
-		<div className="navbar bg-base-100 fixed shadow-md">
-			<div className="flex-1">
-				<a>
-					<img className="w-1/5 cursor-pointer hover:animate-spin" src={logo} />
-				</a>
-			</div>
-			<div className="flex-none">
-				<ul className="menu menu-vertical lg:menu-horizontal px-1">
-					<li className="hover-bordered">
-						<a>Sobre</a>
-					</li>
-					<li className="hover:font-black">
-						<a>Serviços</a>
-					</li>
-					<li className="hover-bordered">
-						<a>L&A Autopeças</a>
-					</li>
-					<li className="hover-bordered">
-						<a>Contato</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	)
+	return <h1>Header</h1>
 }
