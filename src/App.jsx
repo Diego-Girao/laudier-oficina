587c8d57978c617a44ed3c092abd724362941c13
diegogirao <diegoramiro25@gmail.com>
2023-04-24 20:23:36 +0100

    Feat import Navbar and Outlet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 	return (
 		<div>
+			<Navbar />
 			<Header />
 			<Hero />
+			<Outlet />
 			<Footer />
 		</div>
 	)
