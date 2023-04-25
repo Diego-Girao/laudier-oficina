31f9d86907406bad74e6a8c467a9c307e4738c34
diegogirao <diegoramiro25@gmail.com>
2023-04-25 17:41:06 +0100

    Style hover effect and bg color navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,24 +3,24 @@ import logo from "/src/assets/images/logo_oficina.svg"
 
 export default function Navbar() {
 	return (
-		<div className="navbar bg-base-100 fixed shadow-md">
+		<div className="navbar bg-white fixed z-10 shadow-lg">
 			<div className="flex-1">
 				<a>
-					<img className="w-1/5 cursor-pointer hover:animate-spin" src={logo} />
+					<img className="w-1/6 cursor-pointer hover:animate-spin" src={logo} />
 				</a>
 			</div>
 			<div className="flex-none">
 				<ul className="menu menu-vertical lg:menu-horizontal px-1">
-					<li className="hover-bordered">
+					<li className="hover:font-black">
 						<Link to="/about">Sobre</Link>
 					</li>
 					<li className="hover:font-black">
 						<Link to="/repair">Serviços</Link>
 					</li>
-					<li className="hover-bordered">
+					<li className="hover:font-black">
 						<Link to="/store">L&A Autopeças</Link>
 					</li>
-					<li className="hover-bordered">
+					<li className="hover:font-black">
 						<Link to="/contact">Contato</Link>
 					</li>
 				</ul>
