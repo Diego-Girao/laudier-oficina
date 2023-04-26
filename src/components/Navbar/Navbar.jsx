30cb970f2f1daf1ca69cd1cbba90994f8f53fc32
diegogirao <diegoramiro25@gmail.com>
2023-04-26 17:51:55 +0100

    Style correct classname and adjust responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,17 @@ import logo from "/src/assets/images/logo_oficina.svg"
 
 export default function Navbar() {
 	return (
-		<div className="navbar bg-white fixed z-10 shadow-lg">
+		<div className="navbar bg-white z-10 shadow-lg">
 			<div className="flex-1">
 				<a>
 					<img
-						className="w-1/6 cursor-pointer hover:animate-pulse"
+						className="w-1/3 cursor-pointer lg:w-1/6 hover:animate-pulse"
 						src={logo}
 					/>
 				</a>
 			</div>
 			<div className="flex-none">
-				<ul className="menu menu-vertical lg:menu-horizontal px-1">
+				<ul className="p-1 gap-2 lg:menu lg:menu-horizontal">
 					<li className="hover:font-black">
 						<Link to="/about">Sobre</Link>
 					</li>
