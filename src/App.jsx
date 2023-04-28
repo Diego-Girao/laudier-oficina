750e2ff8f61bfdf80f77ea0252dbd0d7b0ff24c8
diegogirao <diegoramiro25@gmail.com>
2023-04-28 21:23:13 +0100

    Update remove header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
 
@@ -8,7 +7,6 @@ function App() {
 	return (
 		<div>
 			<Navbar />
-			<Header />
 			<Hero />
 			<Outlet />
 			<Footer />
