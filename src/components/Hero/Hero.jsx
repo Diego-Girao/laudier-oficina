87343eb442b7db140609fc0b87f5e209f03c25da
diegogirao <diegoramiro25@gmail.com>
2023-04-28 21:24:21 +0100

    Update image carousel

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,31 @@
 export default function Hero() {
 	return (
 		<>
-			<div className="carousel w-full md:w-full">
+			<div className="carousel z-0 w-full md:w-full">
 				<div id="item1" className="carousel-item w-full">
-					<img src="src/assets/images/tool.jpg" className="w-full" />
+					<img src="src/assets/images/teste.jpg" className="w-full" />
 				</div>
 				<div id="item2" className="carousel-item w-full">
-					<img src="src/assets/images/tool.jpg" className="w-full" />
+					<img src="src/assets/images/teste.jpg" className="w-full" />
 				</div>
 				<div id="item3" className="carousel-item w-full">
-					<img src="src/assets/images/tool.jpg" className="w-full" />
+					<img src="src/assets/images/teste.jpg" className="w-full" />
 				</div>
 				<div id="item4" className="carousel-item w-full">
-					<img src="src/assets/images/tool.jpg" className="w-full" />
+					<img src="src/assets/images/teste.jpg" className="w-full" />
 				</div>
 			</div>
 			<div className="flex justify-center w-full py-2 gap-2">
-				<a href="#item1" className="btn btn-circle btn-sm">
+				<a href="#item1" className="btn btn-outline btn-xs">
 					1
 				</a>
-				<a href="#item2" className="btn  btn-circle btn-sm">
+				<a href="#item2" className="btn btn-outline btn-xs">
 					2
 				</a>
-				<a href="#item3" className="btn  btn-circle btn-sm">
+				<a href="#item3" className="btn  btn-outline btn-xs">
 					3
 				</a>
-				<a href="#item4" className="btn  btn-circle btn-sm">
+				<a href="#item4" className="btn  btn-outline btn-xs">
 					4
 				</a>
 			</div>
