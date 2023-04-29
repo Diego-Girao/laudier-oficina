0fbabf1b08a4e4cc672c5274baafb96e93b82b7b
diegogirao <diegoramiro25@gmail.com>
2023-04-29 16:46:36 +0100

    Style change logo, colors and size and responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 import { Link } from "react-router-dom"
-import logo from "/src/assets/images/logo_oficina.svg"
+import logo from "/src/assets/images/logo_oficina.png"
 
 export default function Navbar() {
 	return (
-		<div className="navbar z-10 bg-black shadow-lg shadow-yellow-600">
+		<div className="navbar z-10 bg-black shadow-2xl shadow-black">
 			<div className="flex-1">
-				<a>
+				<a href="/">
 					<img
-						className="w-1/3 cursor-pointer bg-white rounded-lg lg:w-1/12 hover:animate-pulse"
+						className="w-10/12 cursor-pointer lg:w-1/2 hover:animate-pulse"
 						src={logo}
 					/>
 				</a>
 			</div>
 			<div className="flex-none">
-				<ul className="p-1 gap-2 lg:menu lg:menu-horizontal">
-					<li className="hover:font-black">
+				<ul className="p-1 gap-2 menu lg:menu-horizontal lg:text-2xl">
+					<li className="text-gray-700 font-bold hover:text-red-700 hover:font-bold hover-bordered">
 						<Link to="/about">Sobre</Link>
 					</li>
-					<li className="hover:font-black">
+					<li className="text-gray-700 font-bold hover:text-red-700 hover:font-bold hover-bordered">
 						<Link to="/repair">Serviços</Link>
 					</li>
-					<li className="hover:font-black">
+					<li className="text-gray-700 font-bold hover:text-red-700 hover:font-bold hover-bordered">
 						<Link to="/store">L&A Autopeças</Link>
 					</li>
-					<li className="hover:font-black">
+					<li className="text-gray-700 font-bold hover:text-red-700 hover:font-bold hover-bordered">
 						<Link to="/contact">Contato</Link>
 					</li>
 				</ul>
