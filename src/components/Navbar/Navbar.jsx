87738e77846e9e0a990053329c7be6f43b61c938
diegogirao <diegoramiro25@gmail.com>
2023-05-04 12:46:59 +0100

    Update remove commented code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -68,31 +68,3 @@ export default function Navbar() {
 		</nav>
 	)
 }
-// <div className="navbar z-10 bg-black shadow-2xl shadow-black">
-// 	<div className="flex-1">
-// 		<a href="/">
-// 			<img
-// 				className="w-10/12 cursor-pointer lg:w-1/2 hover:animate-pulse"
-// 				src={logo}
-// 			/>
-// 		</a>
-// 	</div>
-// <div className="flex">
-// 	<ul className="p-1 hidden md:flex md:gap-6 md:text-xl md:flex-row">
-// 		<li className="text-gray-700 font-bold p-3 active:translate-y-1 hover:ease-in-out hover:duration-150 hover:bg-gray-700 hover:text-error hover:rounded-md hover:bg-opacity-30">
-// 			<Link to="/about">Sobre</Link>
-// 		</li>
-// 		<li className="text-gray-700 font-bold p-3 active:translate-y-1 hover:ease-in-out hover:duration-150 hover:bg-gray-700 hover:text-error hover:rounded-md hover:bg-opacity-30">
-// 			<Link to="/repair">Serviços</Link>
-// 		</li>
-// 		<li className="text-gray-700 font-bold p-3 active:translate-y-1 hover:ease-in-out hover:duration-150 hover:bg-gray-700 hover:text-error hover:rounded-md hover:bg-opacity-30">
-// 			<Link to="/store">L&A Autopeças</Link>
-// 		</li>
-// 		<li className="text-gray-700 font-bold p-3 active:translate-y-1 hover:ease-in-out hover:duration-150 hover:bg-gray-700 hover:text-error hover:rounded-md hover:bg-opacity-30">
-// 			<Link to="/contact">Contato</Link>
-// 		</li>
-// 	</ul>
-// </div>
-// 		</div>
-// 	)
-// }
