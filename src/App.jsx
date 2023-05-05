d68fd09ebfdfc0672c22fc859b45be0e222b40cd
diegogirao <diegoramiro25@gmail.com>
2023-05-05 21:34:40 +0100

    Update remove hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer/Footer"
-import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 	return (
 		<div>
 			<Navbar />
-			{/* <Hero /> */}
 			<Outlet />
 			<Footer />
 		</div>
