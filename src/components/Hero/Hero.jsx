67e39014a416807748f0e8f2a2a330199688c86e
diegogirao <diegoramiro25@gmail.com>
2023-05-05 21:36:11 +0100

    Update remove commented code

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,61 +20,6 @@ export default function Hero() {
 					</div>
 				</div>
 			</div>
-
-			{/* <div className="card lg:card-side lg:w-fit lg:mt-3 bg-slate-500 shadow-xl shadow-gray-900">
-				<figure>
-					<img
-						src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-						alt="Tools"
-					/>
-				</figure>
-				<div className="card-body">
-					<h2 className="card-title">New album is released!</h2>
-					<p>Click the button to listen on Spotiwhy app.</p>
-				</div>
-			</div>
-			<div className="card lg:card-side lg:w-fit lg:mt-3 bg-slate-500 shadow-xl shadow-gray-900">
-				<figure>
-					<img
-						src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-						alt="Tools"
-					/>
-				</figure>
-				<div className="card-body">
-					<h2 className="card-title">New album is released!</h2>
-					<p>Click the button to listen on Spotiwhy app.</p>
-				</div>
-			</div> */}
 		</>
-		// <>
-		// 	<div className="carousel z-0 w-full">
-		// 		<div id="item1" className="carousel-item w-full">
-		// 			<img src="src/assets/images/teste.jpg" className="w-full" />
-		// 		</div>
-		// 		<div id="item2" className="carousel-item w-full">
-		// 			<img src="src/assets/images/teste.jpg" className="w-full" />
-		// 		</div>
-		// 		<div id="item3" className="carousel-item w-full">
-		// 			<img src="src/assets/images/teste.jpg" className="w-full" />
-		// 		</div>
-		// 		<div id="item4" className="carousel-item w-full">
-		// 			<img src="src/assets/images/teste.jpg" className="w-full" />
-		// 		</div>
-		// 	</div>
-		// 	<div className="flex justify-center w-full py-2 gap-2">
-		// 		<a href="#item1" className="btn btn-outline btn-xs">
-		// 			1
-		// 		</a>
-		// 		<a href="#item2" className="btn btn-outline btn-xs">
-		// 			2
-		// 		</a>
-		// 		<a href="#item3" className="btn  btn-outline btn-xs">
-		// 			3
-		// 		</a>
-		// 		<a href="#item4" className="btn  btn-outline btn-xs">
-		// 			4
-		// 		</a>
-		// 	</div>
-		// </>
 	)
 }
