70edf4b67e791466be67116243626c768a5d0038
diegogirao <diegoramiro25@gmail.com>
2023-05-08 17:52:30 +0100

    Update add text

--- src/routes/Store/Store.jsx
+++ src/routes/Store/Store.jsx
@@ -1,3 +1,11 @@
 export default function Store() {
-	return <div>Store</div>
+	return (
+		<div>
+			<h1>SERVIÇOS AUTOMOTIVOS E AUTO PEÇAS</h1>
+			<p>
+				Profissional especializado em Mecânica Automotiva há mais de 10 anos.
+				Venda de peças automotivas. Qualidade e eficiência é aqui.
+			</p>
+		</div>
+	)
 }
