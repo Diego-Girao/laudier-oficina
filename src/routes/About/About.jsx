7bc957274cc7b6c8a5e4ffe2c165714466a7cb51
diegogirao <diegoramiro25@gmail.com>
2023-05-09 21:34:20 +0100

    Feat add content

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -3,7 +3,32 @@ import Hero from "../../components/Hero/Hero"
 export default function About() {
 	return (
 		<>
-			<Hero />
+			<section>
+				<h1>Missão, Visão e Valores</h1>
+
+				<h2>MISSÃO</h2>
+				<p>
+					A nossa missão é garantir a excelência em auto peças na
+					comercialização e serviços para o cliente e especialistas em reparação
+					automotiva potencializando valor e sustentabilidade do negócio.
+				</p>
+
+				<h2>VISÃO</h2>
+				<p>
+					Visão da Laudier Jr Reparação Automotiva é: tornar-se uma empresa
+					líder no mercado automotivo e no mercado de reposição da linha leve no
+					Rio de Janeiro até 2024 e ser reconhecida pela excelência nos serviços
+					oferecidos ao mercado. Essa visão será obtida nacionalmente até 2030.
+				</p>
+
+				<h2>VALORES</h2>
+				<ul>
+					<li>Ética e transparência nos negócios.</li>
+					<li>Busca por mudanças contínuas.</li>
+					<li>Respeito por vendedores e clientes.</li>
+					<li>Excelência no atendimento e serviços.</li>
+				</ul>
+			</section>
 		</>
 	)
 }
