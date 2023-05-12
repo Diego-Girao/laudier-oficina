7832e358582bfafca61012c698c44dfda199e7b8
diegogirao <diegoramiro25@gmail.com>
2023-05-12 22:19:47 +0100

    Style create cards and texts

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -1,3 +1,5 @@
+import { GearFine } from "@phosphor-icons/react"
+
 export default function About() {
 	return (
 		<>
@@ -7,32 +9,80 @@ export default function About() {
 					backgroundImage: `url("https://images.unsplash.com/photo-1487754180451-c456f719a1fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG1vdG9yJTIwZGUlMjBjYXJyb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")`,
 				}}
 			>
-				<div className="flex flex-col w-full lg:flex-row">
-					<div className="grid mx-4 shadow-md shadow-primary flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-						<h2 className="mb-1 text-2xl font-bold text-left">MISSÃO</h2>
-						<p className="py-1 px-2 text-justify">
-							A nossa missão é garantir a excelência em auto peças na
-							comercialização e serviços para o cliente e especialistas em
-							reparação automotiva potencializando valor e sustentabilidade do
-							negócio.
-						</p>
-					</div>
-					<div className="divider lg:divider-horizontal"></div>
-					<div className="grid mx-4 shadow-md shadow-primary flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-						<h2 className="mb-1 text-2xl font-bold">👀️VISÃO</h2>
-						<p className="py-1 px-2 text-xl text-justify">
-							Visão da Laudier Jr Reparação Automotiva é: tornar-se uma empresa
-							líder no mercado automotivo e no mercado de reposição da linha
-							leve no Rio de Janeiro até 2024 e ser reconhecida pela excelência
-							nos serviços oferecidos ao mercado. Essa visão será obtida
-							nacionalmente até 2030.
-						</p>
-					</div>
-				</div>
 				<div className="hero-overlay bg-opacity-60"></div>
-				<div className="hero-content text-center text-neutral-content -mt-96">
-					<div className="max-w-md">
-						<h1 className="mb-5 text-5xl font-bold">Missão, Visão e Valores</h1>
+				<div className="flex flex-col w-full justify-between p-4 text-justify md:flex-row">
+					{/* CARD 1 */}
+					<div className="card card-bordered w-96 bg-base-100 shadow-lg shadow-black">
+						<figure>
+							<img
+								src="https://images.unsplash.com/photo-1498887960847-2a5e46312788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNhciUyMHByb2JsZW1zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+								alt="Shoes"
+							/>
+						</figure>
+						<div className="card-body">
+							<h2 className="card-title p-2 text-4xl bg-gray-200 rounded-md shadow-md shadow-gray-600 mb-2">
+								🤝️ Missão
+							</h2>
+							<p className="text-lg">
+								A nossa missão é garantir a excelência em auto peças na
+								comercialização e serviços para o cliente e especialistas em
+								reparação automotiva potencializando valor e sustentabilidade do
+								negócio.
+							</p>
+						</div>
+					</div>
+					{/* CARD 2 */}
+					<div className="card card-bordered w-96 bg-base-100 shadow-lg shadow-black">
+						<figure>
+							<img
+								src="https://images.unsplash.com/photo-1498887960847-2a5e46312788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNhciUyMHByb2JsZW1zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+								alt="Shoes"
+							/>
+						</figure>
+						<div className="card-body">
+							<h2 className="card-title p-2 text-4xl bg-gray-200 rounded-md shadow-md shadow-gray-600 mb-2">
+								👀️ Visão
+							</h2>
+							<p className="text-lg">
+								Visão da Laudier Jr Reparação Automotiva é, tornar-se uma
+								empresa líder no mercado automotivo e no mercado de reposição da
+								linha leve no Rio de Janeiro até 2024 e ser reconhecida pela
+								excelência nos serviços oferecidos ao mercado. Essa visão será
+								obtida nacionalmente até 2030.
+							</p>
+						</div>
+					</div>
+					{/* CARD 3 */}
+					<div className="card card-bordered w-96 bg-base-100 shadow-lg shadow-black">
+						<figure>
+							<img
+								src="https://images.unsplash.com/photo-1498887960847-2a5e46312788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNhciUyMHByb2JsZW1zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+								alt="Shoes"
+							/>
+						</figure>
+						<div className="card-body">
+							<h2 className="card-title p-2 text-4xl bg-gray-200 rounded-md shadow-md shadow-gray-600 mb-2">
+								🎖️ Valores
+							</h2>
+							<ul>
+								<li className="flex items-center mb-4 text-lg gap-1">
+									<GearFine className="animate-spin" />
+									Ética e transparência nos negócios.
+								</li>
+								<li className="flex items-center mb-4 text-lg gap-1">
+									<GearFine className="animate-spin" />
+									Busca por mudanças contínuas.
+								</li>
+								<li className="flex items-center mb-4 text-lg gap-1">
+									<GearFine className="animate-spin" />
+									Respeito por vendedores e clientes.
+								</li>
+								<li className="flex items-center text-lg gap-1">
+									<GearFine className="animate-spin" />
+									Excelência no atendimento e serviços.
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
