5d4f47339da322c18c17f92d0539546e00c847c0
diegogirao <diegoramiro25@gmail.com>
2023-05-14 22:10:05 +0100

    Style add text

--- src/routes/Repair/Repair.jsx
+++ src/routes/Repair/Repair.jsx
@@ -3,7 +3,13 @@ import Hero from "../../components/Hero/Hero"
 export default function Repair() {
 	return (
 		<>
-			<Hero />
+			<h1>Manutenção preventiva é aqui ! 👊</h1>
+
+			<p>
+				Funcionamento: Segunda à Sábado de 8h às 18h. Domingos e Feriados de 8h
+				às 12h. Profissional qualificado, produtos de qualidade e com aquele
+				precinho e condições facilitadas! Venham conferir!
+			</p>
 		</>
 	)
 }
