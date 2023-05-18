59575467faa151629103fda9c727bc53c5dfa987
diegogirao <diegoramiro25@gmail.com>
2023-05-18 20:34:53 +0100

    Style change second image

--- src/routes/Repair/Repair.jsx
+++ src/routes/Repair/Repair.jsx
@@ -7,7 +7,7 @@ const images = [
 		text: "Texto sobre a imagem 1",
 	},
 	{
-		src: "src/assets/images/tool.jpg",
+		src: "src/assets/images/WhatsApp Image 2023-04-15 at 19.55.16 (1).jpeg",
 		alt: "Image 2",
 		text: "Texto sobre a imagem 2",
 	},
@@ -28,10 +28,10 @@ export default function Repair() {
 			</div>
 			{/* 
 			<p>
-				Funcionamento: Segunda à Sábado de 8h às 18h. Domingos e Feriados de 8h
-				às 12h. Profissional qualificado, produtos de qualidade e com aquele
-				precinho e condições facilitadas! Venham conferir!
-			</p> */}
+			Funcionamento: Segunda à Sábado de 8h às 18h. Domingos e Feriados de 8h
+			às 12h. Profissional qualificado, produtos de qualidade e com aquele
+			precinho e condições facilitadas! Venham conferir!
+		</p> */}
 		</>
 	)
 }
