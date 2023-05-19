90fb209f8dcb87173c3ef0130a506ef602327998
diegogirao <diegoramiro25@gmail.com>
2023-05-19 21:04:21 +0100

    Style change to carousel mode

--- src/routes/Repair/Repair.jsx
+++ src/routes/Repair/Repair.jsx
@@ -1,31 +1,42 @@
-import Carousel from "../../components/Hero/Carousel"
-
-const images = [
-	{
-		src: "src/assets/images/tool.jpg",
-		alt: "Image 1",
-		text: "Texto sobre a imagem 1",
-	},
-	{
-		src: "src/assets/images/WhatsApp Image 2023-04-15 at 19.55.16 (1).jpeg",
-		alt: "Image 2",
-		text: "Texto sobre a imagem 2",
-	},
-	{
-		src: "src/assets/images/tool.jpg",
-		alt: "Image 3",
-		text: "Texto sobre a imagem 3",
-	},
-]
-
 export default function Repair() {
 	return (
 		<>
-			{/* <h1>Manutenção preventiva é aqui ! 👊</h1> */}
-			<div>
-				<h1>Meu Carousel</h1>
-				<Carousel images={images} />
+			<div className="carousel w-full">
+				<div id="item1" className="carousel-item w-full">
+					<img src="src/assets/images/tool.jpg" className="w-full" />
+				</div>
+				<div id="item2" className="carousel-item w-full">
+					<img
+						src="src/assets/images/WhatsApp Image 2023-04-15 at 19.55.16 (1).jpeg"
+						className="w-full"
+					/>
+				</div>
+				<div id="item3" className="carousel-item w-full">
+					<img src="src/assets/images/tool.jpg" className="w-full" />
+				</div>
+				<div id="item4" className="carousel-item w-full">
+					<img
+						src="src/assets/images/WhatsApp Image 2023-04-15 at 19.55.16 (1).jpeg"
+						className="w-full"
+					/>
+				</div>
 			</div>
+			<div className="flex justify-center w-full py-2 gap-2">
+				<a href="#item1" className="btn btn-xs">
+					1
+				</a>
+				<a href="#item2" className="btn btn-xs">
+					2
+				</a>
+				<a href="#item3" className="btn btn-xs">
+					3
+				</a>
+				<a href="#item4" className="btn btn-xs">
+					4
+				</a>
+			</div>
+
+			{/* <h1>Manutenção preventiva é aqui ! 👊</h1> */}
 			{/* 
 			<p>
 			Funcionamento: Segunda à Sábado de 8h às 18h. Domingos e Feriados de 8h
