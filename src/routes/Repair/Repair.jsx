ef743b5f85896fe21c18ce66e1487a26e62df717
diegogirao <diegoramiro25@gmail.com>
2023-06-04 19:37:10 +0100

    fix: changes card 5 image alt

--- src/routes/Repair/Repair.jsx
+++ src/routes/Repair/Repair.jsx
@@ -131,7 +131,7 @@ export default function About() {
 						<figure>
 							<img
 								src="https://images.unsplash.com/photo-1670764169470-bd2f737f9248?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-								alt="Motor do carro vista frontal das correias"
+								alt="Zoom da parte de cima de uma vela veicular"
 							/>
 						</figure>
 						<div className="card-body">
