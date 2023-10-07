741a708a5af5d47ca3f8537d302496075c54ac0d
diegogirao <diegoramiro25@gmail.com>
2023-10-07 17:56:02 +0100

    fix card position

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -15,13 +15,13 @@ export default function Contact() {
 	return (
 		<>
 			<div
-				className="hero min-h-screen -mt-2"
+				className="hero min-h-screen -mt-48"
 				style={{
 					backgroundImage: `url("https://plus.unsplash.com/premium_photo-1658527057580-f473648a16d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZW5naW5lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")`,
 				}}
 			>
 				<div className="hero-overlay bg-opacity-60"></div>
-				<section className="hero-content -mt-28 flex-col text-xl font-bold text-white md:flex-row md:text-3xl">
+				<section className="hero-content mt-36 flex-col text-xl font-bold text-white md:flex-row md:text-3xl">
 					<p
 						data-aos={"fade-right"}
 						data-aos-delay={"200"}
