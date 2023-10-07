fcf6f1dfa6f29246308e6201decaeebbe455b2fd
diegogirao <diegoramiro25@gmail.com>
2023-10-07 22:05:13 +0100

    feat created home card component

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 
-import imgBg from "/src/assets/images/owner.png"
+import imgOwnerStore from "/src/assets/images/owner_store.png"
 
 import AOS from "aos"
 import "aos/dist/aos.css"
@@ -12,21 +12,24 @@ export default function Home() {
 	}, [])
 	return (
 		<div
-			className="hero relative -z-10 place-items-start"
+			className="hero min-h-screen -mt-2 relative -z-10"
 			style={{
-				backgroundImage: `url("https://plus.unsplash.com/premium_photo-1658527057580-f473648a16d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZW5naW5lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")`,
+				backgroundImage: `url("https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60")`,
 			}}
 		>
 			<div className="hero-overlay bg-opacity-80"></div>
-			<div
-				data-aos={"fade-right"}
-				data-aos-easing={"ease-in-sine"}
-				data-aos-duration={"900"}
-				className="-mb-14"
-			>
-				<figure>
-					<img src={imgBg} alt="Dono da loja" />
-				</figure>
+			<div className="card mt-8 justify-center md:mt-8 lg:w-full">
+				{/* CARD 1 */}
+				<div
+					data-aos={"flip-left"}
+					data-aos-duration={"800"}
+					data-aos-easing={"ease-in-out"}
+					className="mb-8 bg-black bg-opacity-40 shadow-lg shadow-black"
+				>
+					<figure>
+						<img src={imgOwnerStore} alt="Mural de ferramentas" />
+					</figure>
+				</div>
 			</div>
 		</div>
 	)
