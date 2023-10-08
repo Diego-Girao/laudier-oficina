c7dfa7a5cf47a799870b9ef0b176f7ec9608ba4f
diegogirao <diegoramiro25@gmail.com>
2023-10-08 14:54:19 +0100

    fix removed negative zindex

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
 	return (
 		<>
 			<div
-				className="hero min-h-screen -mt-28 relative -z-10"
+				className="hero min-h-screen -mt-28 relative"
 				style={{
 					backgroundImage: `url("https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60")`,
 				}}
