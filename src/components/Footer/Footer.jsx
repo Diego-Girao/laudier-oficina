d3481d2f0a972225c14c20de676b862b6968a015
diegogirao <diegoramiro25@gmail.com>
2023-10-08 14:07:53 +0100

    fix add aria label at anchor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,11 @@ export default function Footer() {
 			</p>
 			<div className="grid-flow-col gap-2 justify-self-center md:items-center md:place-self-center md:justify-self-end">
 				<p>Por Diego Ramiro</p>
-				<a href={github} target="_blank">
+				<a
+					href={github}
+					target="_blank"
+					aria-label="Saiba sobre o projeto e outros mais"
+				>
 					<svg
 						className="fill-current bg-neutral rounded-full opacity-55 w-5 md:w-6 active:translate-y-1 hover:bg-black hover:fill-slate-600 hover:opacity-100"
 						xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +26,10 @@ export default function Footer() {
 						<path d="M208.3,75.68A59.74,59.74,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58,58,0,0,0,208.3,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.76,41.76,0,0,1,200,104Z"></path>
 					</svg>
 				</a>
-				<a href="mailto:diegoramiro25@gmail.com">
+				<a
+					href="mailto:diegoramiro25@gmail.com"
+					aria-label="Send an e-mail to developer"
+				>
 					<svg
 						className="fill-current opacity-55 w-5 md:w-6 active:translate-y-1 hover:fill-white hover:opacity-100"
 						xmlns="http://www.w3.org/2000/svg"
